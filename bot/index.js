6eb087974142d6646503da7e9bf35471eba6377f
Scott MacWatters <code@macwatters.com>
2017-02-25 09:20:05 -0600

    Iterating and seeing what session datas are available

--- bot/index.js
+++ bot/index.js
@@ -25,6 +25,25 @@ var bot = new builder.UniversalBot(connector);
 app.post('/api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
-  console.log(JSON.stringify(session.message,null,2));
+  console.log(session.message.textFormat);
+  console.log(session.message.text);
+  var mentions = [];
+  for(var e in session.message.entities){
+    if(type === 'mention'){
+      mentions.push(e.mentioned);
+    }
+  }
+  console.log(mentions);
+  console.log(session.message.sourceEvent.teamsChannelId);
+  console.log(session.message.sourceEvent.teamsTeamId);
+  console.log(session.message.sourceEvent.tenant.id);
+  console.log(session.message.address.id);
+  console.log(session.message.address.channelId);
+  console.log(session.message.address.conversation.isGroup);
+  console.log(session.message.address.conversation.id);
+  console.log(session.message.user.id);
+  console.log(session.message.user.name);
+
+
   session.send("Aaaay");
 });
