0adeb4de69706dee446e438ccfafd84f9d9e1e8b
Scott MacWatters <code@macwatters.com>
2017-02-25 08:40:42 -0600

    Iterating and seeing what session datas are available

--- bot/index.js
+++ bot/index.js
@@ -25,6 +25,6 @@ var bot = new builder.UniversalBot(connector);
 app.post('/api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
-  console.log(JSON.stringify(session,null,2));
+  console.log(JSON.stringify(session.message,null,2));
   session.send("Hello World");
 });
