2d2e9aafcb9d71ecc25bd02bebf181f91f87d5cf
Scott MacWatters <code@macwatters.com>
2017-02-25 08:04:22 -0600

    Test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var bot = new builder.UniversalBot(connector);
 
 app.post('/api/messages', connector.listen());
 
-app.get('/test', function(req,res){
+app.get('/test/', function(req,res){
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({message:"Success"}));
   req.next();
